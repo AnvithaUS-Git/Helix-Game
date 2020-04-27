295ce1ba379d30d13066713f0e65ce31e65c5133
Anvitha <anvithaus07@gmail.com>
2020-04-27 19:38:56 +0530

    Reset camera after player death

--- Assets/HelixJump/Scripts/HJBall_CameraFollower.cs
+++ Assets/HelixJump/Scripts/HJBall_CameraFollower.cs
@@ -11,11 +11,21 @@ namespace HelixJump
         [SerializeField] private float mSmoothness;
         private float mOffsetBetCamAndBall;
         private Vector3 mVelocity;
+        private Vector3 mInitialCameraPos;
         private void Awake()
         {
             mOffsetBetCamAndBall = transform.position.y - mBall.position.y;
+            mInitialCameraPos = transform.position;
+        }
+        private void OnEnable()
+        {
+            HJGameEventHandler.Instance().OnPlayerDeathEvent += ResetCameraPos;
         }
 
+        private void OnDisable()
+        {
+            HJGameEventHandler.Instance().OnPlayerDeathEvent -= ResetCameraPos;
+        }
         private void Start()
         {
             mCamera.backgroundColor = HJConfigManager.Instance().GetBackgroundColorForLevel(HJPlayerScoreAndLevelManager.Instance().CurrentLevel);
@@ -30,6 +40,10 @@ namespace HelixJump
                 //this.transform.position = Vector3.SmoothDamp(transform.position, targetPos, ref mVelocity, mSmoothness);
             }
 
+        }
+        void ResetCameraPos()
+        {
+            transform.position = mInitialCameraPos;
         }
     }
 }
